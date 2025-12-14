feb8bef7ddd5c5282b15a40e7938abc91365bf6b
ahmethamdi <ahmethamdik8@gmail.com>
2025-12-15 00:41:09 +0100

    sidebar
    sidebar

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -22,9 +22,11 @@ export default function App() {
     <AppProvider isEmbeddedApp apiKey={apiKey}>
       <NavMenu>
         <Link to="/app" rel="home">
-          Home
+          Dashboard
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/delivery-rules">Delivery Rules</Link>
+        <Link to="/app/holidays">Holidays & Weekends</Link>
+        <Link to="/app/settings">Settings</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
